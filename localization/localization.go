0a4ddaa4aa4ae6b592d797cde9d1befc36a983e7
Jorge Luis Rodríguez González <egroj97@gmail.com>
2022-06-12 14:48:25 -0400

    proof of concept

---
+++ localization/localization.go
@@ -0,0 +1,16 @@
+package localization
+
+const (
+	HomeButtonText = iota
+	PatientsButtonText
+)
+
+var Spanish = []string{
+	"Inicio",
+	"Pacientes",
+}
+
+var English = []string{
+	"Home",
+	"Patients",
+}
