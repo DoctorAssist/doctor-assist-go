e815415785f1e1d2d98f777feaaa7de4c9f9d816
Jorge Luis Rodríguez González <egroj97@gmail.com>
2022-06-26 18:50:08 -0400

    SQL queries support

--- core/sqlwrapper/sqlwrapper.go
+++ core/sqlwrapper/sqlwrapper.go
@@ -1,24 +1,62 @@
 package sqlwrapper
 
 import (
-	"database/sql"
+	"context"
+
+	"github.com/jmoiron/sqlx"
 
 	"github.com/DoctorAssist/doctor-assist-go/models/patients"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type SQLWrapper struct {
-	*sql.DB
+	*sqlx.DB
 }
 
 func New(driverName, dataSourceName string) (wrapper *SQLWrapper, err error) {
 	wrapper = new(SQLWrapper)
-	wrapper.DB, err = sql.Open(driverName, dataSourceName)
+	wrapper.DB, err = sqlx.Open(driverName, dataSourceName)
 	return wrapper, err
 }
 
-func (*SQLWrapper) InsertPatientData(patients.Patient) error
-func (*SQLWrapper) FetchPatient(string) (patients.Patient, error)
-func (*SQLWrapper) FetchAllPatients(string) ([]patients.Patient, error)
-func (*SQLWrapper) UpdatePatient(patients.Patient) (error)
-func (*SQLWrapper) RemovePatient(string) error
+func (db *SQLWrapper) InsertPatientData(ctx context.Context, p patients.Patient) error {
+	_, err := db.NamedExecContext(
+		ctx,
+		"INSERT INTO patient (name, sex, age, id, phone, email, address) "+
+			"VALUES (:name, :sex, :age, :id, :phone, :email, :address)",
+		&p,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *SQLWrapper) FetchPatient(ctx context.Context,
+	id string) (patients.Patient, error) {
+	p := patients.Patient{}
+	err := db.GetContext(ctx, &p, "SELECT * FROM patients WHERE id = $1", id)
+	if err != nil {
+		return patients.Patient{}, err
+	}
+
+	return p, nil
+}
+
+func (db *SQLWrapper) FetchAllPatients(ctx context.Context) ([]patients.Patient, error) {
+	ps := []patients.Patient{}
+	err := db.SelectContext(ctx, &p, "SELECT * FROM patients WHERE id = $1", id)
+	if err != nil {
+		return patients.Patient{}, err
+	}
+
+	return p, nil
+}
+
+func (db *SQLWrapper) UpdatePatient(patients.Patient) error {
+
+}
+
+func (db *SQLWrapper) RemovePatient(string) error {
+
+}
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564 // indirect
